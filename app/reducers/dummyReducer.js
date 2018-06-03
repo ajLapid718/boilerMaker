b1d2ba16a64e1e3f803cfef25fb3f56bacd2ff76
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 00:54:04 -0400

    Split and separate reducers

--- app/reducers/dummyReducer.js
+++ app/reducers/dummyReducer.js
@@ -1,17 +1,27 @@
 import { GET_EVERYTHING, GET_ONE_THING } from '../actions/action-creators.js';
+import { combineReducers } from 'redux';
 
-const initialState = {
-  items: [],
-  selectedItem: {}
-}
-
-export function dummyReducer(state = initialState, action) {
+function itemsReducer(state = [], action) {
   switch (action.type) {
     case GET_EVERYTHING:
       return {...state, items: action.payload};
+    default:
+      return state;
+  }
+};
+
+function selectedItemReducer(state = {}, action) {
+  switch (action.type) {
     case GET_ONE_THING:
       return {...state, selectedItem: action.payload};
     default:
       return state;
   }
-}
+};
+
+const rootReducer = combineReducers({
+  items: itemsReducer,
+  item: selectedItemReducer
+});
+
+export default rootReducer;

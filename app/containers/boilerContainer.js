36a55182d46d714e7ca1e3997ec41864df284ec4
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 01:06:20 -0400

    Establish smart container

--- app/containers/boilerContainer.js
+++ app/containers/boilerContainer.js
@@ -3,6 +3,18 @@ import AllItems from '../components/boilerComponent.js';
 import { connect } from 'react-redux';
 import { getEverything } from '../actions/action-creators.js';
 
-const AllItemsContainer = connect(null, null)(AllItems);
+const mapStateToProps = state => {
+  return {
+    allItems: state.items
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    displayAll: () => dispatch(getEverything())
+  }
+}
+
+const AllItemsContainer = connect(mapStateToProps, mapDispatchToProps)(AllItems);
 
 export default AllItemsContainer;

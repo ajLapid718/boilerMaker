cb6cc2042134b92e4a88c820d05c6081f92b919b
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 01:00:47 -0400

    Set up skeleton for smart container

--- app/containers/boilerContainer.js
+++ app/containers/boilerContainer.js
@@ -1,1 +1,8 @@
 // Smart Container Goes Here;
+import AllItems from '../components/boilerComponent.js';
+import { connect } from 'react-redux';
+import { getEverything } from '../actions/action-creators.js';
+
+const AllItemsContainer = connect(null, null)(AllItems);
+
+export default AllItemsContainer;

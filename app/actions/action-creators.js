6833d1307f336477a22cabfcddc000dca2fb7c8e
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 00:42:03 -0400

    Define action types and action creators

--- app/actions/action-creators.js
+++ app/actions/action-creators.js
@@ -1,1 +1,18 @@
 // Action Naming and Action Creating Goes Here;
+
+const GET_EVERYTHING = "GET_EVERYTHING";
+const GET_ONE_THING = "GET_ONE_THING";
+
+function getEverything(everything) {
+  return {
+    type: GET_EVERYTHING,
+    payload: everything
+  }
+};
+
+function getEverything(oneThing) {
+  return {
+    type: GET_ONE_THING,
+    payload: oneThing
+  }
+};

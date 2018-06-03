35ba8add6b7270bff7be3aa1e49299c74bfb5e06
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 01:46:34 -0400

    Dish out a 404 if an API route does not exist

--- server/api/index.js
+++ server/api/index.js
@@ -2,4 +2,10 @@ const apiRouter = require('express').Router();
 
 apiRouter.use('/items', require('./items.router'));
 
+router.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
 module.exports = apiRouter;

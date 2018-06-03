8b51a7eb71df24d98afd0005f55c13cf9c2f0274
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 01:09:16 -0400

    Render smart container

--- app/main.js
+++ app/main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
+import { boilerContainer } from './containers/boilerContainer.js';
 
 ReactDOM.render(
   <Provider store={store}>
     <div>Hello, world!</div>
+    <boilerContainer />
   </Provider>,
   document.getElementById('app')
 );

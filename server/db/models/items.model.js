36a69a1bdbaa150b2275be8815db6b02732b5c49
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 13:47:06 -0400

    Finish Item model fields and export it

--- server/db/models/items.model.js
+++ server/db/models/items.model.js
@@ -1,2 +1,22 @@
 const Sequelize = require('sequelize');
 const db = require('../database.js');
+
+const Item = db.define('item', {
+
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  weight: {
+    type: Sequelize.INTEGER
+  }
+
+})
+
+// Virtuals and Hooks could also be included;
+
+module.exports = Item;

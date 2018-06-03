0fae6726302de66b30d0a95c7ae0e8e3767cf69d
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 01:28:28 -0400

    Install body-parser and use it for parsing middleware

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, './public/assets')));
 
 app.use(morgan('dev');)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('*', function (req, res, next) {

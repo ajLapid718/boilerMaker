a19699dc159f9fbfedf855177b642cf7888653d0
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 00:57:53 -0400

    Make a sample React component

--- app/components/boilerComponent.js
+++ app/components/boilerComponent.js
@@ -1,1 +1,12 @@
 // React Component Goes Here;
+import React, { Component } from 'react';
+
+export default class AllItems extends Component {
+  render() {
+    return (
+      <div>
+        SHOW ALL ITEMS HERE FROM THE STATE WHICH WAS PASSED DOWN FROM THE STORE
+      </div>
+    )
+  }
+}

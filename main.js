966d9834f0b32ef247e3494ff45742a9cf209b88
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 13:53:59 -0400

    Sync your database

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const app = require('./server');
-
+const { db } = require('./server/db/models/index.js');
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
-app.listen(port, function () {
-  console.log("Knock, knock");
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-});
+
+db.sync({force: true})
+  .then(() => {
+    console.log("The database is synced, all tables created!");
+    app.listen(port, function () {
+    console.log("Knock, knock");
+    console.log("Who's there?");
+    console.log(`Your server, listening on port ${port}`);
+  }));
+  .catch(err => console.log(err));

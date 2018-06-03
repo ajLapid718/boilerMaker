683fb628dced2d37d0b7dac4a579b1796941551e
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 00:47:51 -0400

    First Law of Tom regarding initial state

--- app/reducers/dummyReducer.js
+++ app/reducers/dummyReducer.js
@@ -1,3 +1,13 @@
-export function dummyReducer(state = {}, action) {
-  return state;
+import { GET_EVERYTHING, GET_ONE_THING } from '../actions/action-creators.js';
+
+const initialState = {
+  items: [],
+  selectedItem: {}
 }
+
+// export function dummyReducer(state = [], action) {
+//   switch (action.type) {
+//     case GET_EVERYTHING:
+//       return
+//   }
+// }

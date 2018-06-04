6bbdcc56cad319256c431bfc9e8caabe773b86bd
Allan James Lapid <ajLapid718@gmail.com>
2018-06-03 22:44:28 -0400

    Initialize passport in order to consume req session object

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const passport = require('passport');
 
 app.use(express.static(path.join(__dirname, './public/assets')));
 
@@ -16,6 +17,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', require('./apiRouter'));
 
